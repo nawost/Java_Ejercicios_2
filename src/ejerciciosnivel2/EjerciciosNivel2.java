5df28bd44c9af25af54c1f522cd16cec851a3a62
Joan Marí <joan.jms22@gmail.com>
2022-01-14 19:21:14 +0100

    Empezamos escaleras de palabras

---
+++ src/ejerciciosnivel2/EjerciciosNivel2.java
@@ -0,0 +1,30 @@
+package ejerciciosnivel2;
+/**
+ *
+ * @author Joan Marí
+ */
+public class EjerciciosNivel2 {
+
+    public boolean esEscaleraPalabras(char[][] listaPalabras){
+        
+        return true;
+    }
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char [][] listaPalabras = {
+            {'P', 'A', 'T', 'A'}, 
+            {'P', 'A', 'T', 'O'}, 
+            {'R', 'A', 'T', 'O'}, 
+            {'R', 'A', 'M', 'O'}, 
+            {'G', 'A', 'M', 'O'}, 
+            {'G', 'A', 'T', 'O'}, 
+            {'M', 'A', 'T', 'O'}, 
+        };
+    }
+    
+}
