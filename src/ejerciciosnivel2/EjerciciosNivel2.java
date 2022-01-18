c5d8380b209d8af75d693eec5e6bfe6215442cfc
Joan Marí <joan.jms22@gmail.com>
2022-01-18 19:15:55 +0100

    finalizado el ejercicio de ADN

--- src/ejerciciosnivel2/EjerciciosNivel2.java
+++ src/ejerciciosnivel2/EjerciciosNivel2.java
@@ -9,7 +9,7 @@ public class EjerciciosNivel2 {
         for (int i=0; i<listaPalabras.length-1; i++){
             int contador = 0;
             for (int j=0; j<listaPalabras[0].length; j++){
-                if (listaPalabras[i][j] == listaPalabras[i+1][j]){
+                if (listaPalabras[i][j] != listaPalabras[i+1][j]){
                     contador++;
                 }
             }
@@ -20,7 +20,20 @@ public class EjerciciosNivel2 {
         return true;
     }
     
-    
+    private int costeErroresADN (String uno, String dos){
+        int coste = 0;
+        for (int i=0; i<uno.length(); i++){
+            if (uno.charAt(i) == '-' || dos.charAt(i) == '-'){coste +=2;}
+            else{
+            if (uno.charAt(i) == 'C' && dos.charAt(i) != 'G'){coste++;}
+            if (uno.charAt(i) == 'G' && dos.charAt(i) != 'C'){coste++;}
+            if (uno.charAt(i) == 'A' && dos.charAt(i) != 'T'){coste++;}
+            if (uno.charAt(i) == 'T' && dos.charAt(i) != 'A'){coste++;}
+            }
+        }
+        
+        return coste;
+    }
     
     /**
      * @param args the command line arguments
@@ -37,8 +50,14 @@ public class EjerciciosNivel2 {
         };
         EjerciciosNivel2 e = new EjerciciosNivel2();
         System.out.println(e.esEscaleraPalabras(listaPalabras));
-        
-        
+        System.out.println(e.costeErroresADN("ACGT", "TGCA"));
+        System.out.println(e.costeErroresADN("A-C-G-T-ACGT", "TTGGCCAATGCA"));
+        System.out.println(e.costeErroresADN("AAAAAAAA", "TTTATTTT"));
+        System.out.println(e.costeErroresADN("GATTACA", "CTATT-T"));
+        System.out.println(e.costeErroresADN("CAT-TAG-ACT", "GTATATCCAAA"));
+        System.out.println(e.costeErroresADN("--------", "ACGTACGT"));
+        System.out.println(e.costeErroresADN("TAATAA", "ATTATT"));
+        System.out.println(e.costeErroresADN("GGGA-GAATATCTGGACT", "CCCTACTTA-AGACCGGT"));
     }
     
 }
